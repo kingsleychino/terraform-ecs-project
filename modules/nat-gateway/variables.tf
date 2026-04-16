835aa3f56e9438d93e98126816e1671c7aabb8ac
kingsleychino <kingsleychinono@gmail.com>
2026-04-16 03:19:48 +0100

    added nat gateway

---
+++ modules/nat-gateway/variables.tf
@@ -0,0 +1,15 @@
+variable "public_subnet_az1_cidr" {}
+
+variable "internet_gateway" {}
+
+variable "public_subnet_az2_cidr" {}
+
+variable "vpc_id" {}
+
+variable "private_app_subnet_az1_id" {}
+
+variable "private_data_subnet_az1_id" {}
+
+variable "private_app_subnet_az2_id" {}
+
+variable "private_data_subnet_az2_id" {}
