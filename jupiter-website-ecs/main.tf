2f587fa8e65414dd4afa3ccaf282f120731a9198
kingsleychino <kingsleychinono@gmail.com>
2026-04-17 01:36:59 +0100

    creat ACM

--- jupiter-website-ecs/main.tf
+++ jupiter-website-ecs/main.tf
@@ -39,3 +39,9 @@ module "ecs_tasks_execution_role" {
   source       = "../modules/ecs-tasks-execution-role"
   project_name = module.vpc.project_name
 }
+
+module "acm" {
+  source           = "../modules/acm"
+  domain_name      = var.domain_name
+  alternative_name = var.alternative_name
+}
